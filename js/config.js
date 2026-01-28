5cf1e1551ce18bb38e3e32be7697721ddf950382
1bpmro <ti1bpmro@gmail.com>
2026-01-28 12:04:19 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Arquivo central de configuração
+const CONFIG = {
+    URL_GAS: "https://script.google.com/macros/s/AKfycbyGYlOffbQayYfuk42XZAPn8dL9i2S7TU9pUo2ED9yYNixr8Ebg1dOIYR_m4-z00o8TDQ/exec"
+};
